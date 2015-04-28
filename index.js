574e9de25eebffdb8d9c8b9917006a5ea8f5dfe2
Thomas Gideon <thomas@webs.com>
2015-04-28 12:36:18 -0400

    Initial commit.

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', function(req, res) {
+    res.type('text/plain');
+    res.send('Jira Stats');
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Express started on http://localhost:' +
+            app.get('port') + '; press Ctrl-C to terminate.');
+});
