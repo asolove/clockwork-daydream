2316d772d85f51f987e0b6c5023fbfd3b777743e
Thomas Gideon <thomas@webs.com>
2015-04-29 13:17:05 -0400

    Work more with async, start to clean up anticipating richer front end.

--- index.js
+++ index.js
@@ -1,49 +1,38 @@
 var Promise = require('bluebird');
 var express = require('express');
 var handlebars = require('express-handlebars').create({ defaultLayout: 'main' });
-var transitions = require('./transitions');
 var read = require('read');
+var jira = require('./lib/jira');
 
 var app = express();
 
 var asyncRead = Promise.promisify(read);
 
-var setupExpress = function() {
-    // set up express
-    app.engine('handlebars', handlebars.engine);
-    app.set('view engine', 'handlebars');
-    app.set('port', process.env.PORT || 3000);
-    app.use(express.static(__dirname + '/public'));
+// set up express
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public'));
 
-    // routes
-    // TODO move these to their own files, in routes
-    // TODO us fs to load and use routes
-    // default route
-    app.get('/', function(req, res) {
-        res.render('home');
-    });
-
-    // report on dwell time
-    // TODO think about renaming
-    app.get('/transitions/dwell', transitions.dwell);
-
-    // spin up the server
-    app.listen(app.get('port'), function() {
-        console.log('Express started on http://localhost:' +
-                app.get('port') + '; press Ctrl-C to terminate.');
-    });
-};
-
-// need to capture credentials, password is safest interactively
-asyncRead({ prompt:'Please enter your Jira username:' }).then(function(username) {
-    // not sure why read or the promisified version returns an array
-    app.set('username', username[0]);
-    return asyncRead({ prompt:'Please enter your Jira password:', silent: true });
-}).then(function(password) {
-    // not sure why read or the promisified version returns an array
-    app.set('password', password[0]);
-
-    setupExpress();
+// routes
+// default route
+app.get('/', function(req, res) {
+    res.render('home');
 });
 
+// spin up the server
+app.listen(app.get('port'), function() {
+    console.log('Express started on http://localhost:' +
+            app.get('port') + '; press Ctrl-C to terminate.');
+    // need to capture credentials, password is safest interactively
+    asyncRead({ prompt:'Please enter your Jira username:' }).then(function(username) {
+        // not sure why read or the promisified version returns an array
+        app.set('username', username[0]);
+        return asyncRead({ prompt:'Please enter your Jira password:', silent: true });
+    }).then(function(password) {
+        // not sure why read or the promisified version returns an array
+        app.set('password', password[0]);
 
+        app.set('streams', jira.streamField(app.get('username'), app.get('password')));
+    });
+});
