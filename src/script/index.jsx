35cc3e860cbeea495006457a8ff5460e116e0937
Thomas Gideon <thomas@webs.com>
2015-05-04 12:00:33 -0400

    Populate the drop down.

--- src/script/index.jsx
+++ src/script/index.jsx
@@ -11,17 +11,41 @@ if (window) {
 
 
 var ViewSelect = React.createClass({
+    getInitialState: function() {
+        return { views: [] };
+    },
+
     componentDidMount: function() {
-        http.get('/views', function(error, result) {
-            if (error !== null) console.error(error);
-            console.log(result);
+        http.get('/views', function(response) {
+            var data = '';
+
+            response.on('data', function(buf) {
+                data += buf;
+            });
+
+            response.on('end', function() {
+                try {
+                    var result = JSON.parse(data);
+                    this.setState({ views: result });
+                } catch(err) {
+                    console.error(err);
+                }
+            }.bind(this));
+        }.bind(this)).on('error', function(err) {
+            console.error(err);
         });
     },
 
     render: function() {
+        var options = this.state.views.map(function(view) {
+            return (
+                <option value={view.id}>{view.name}</option>
+            );
+        });
         return (
             <select ref="views">
                 <option value="none">Select a View</option>
+                {options}
             </select>
         );
     }
