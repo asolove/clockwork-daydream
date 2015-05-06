731ea59f1927a937251ee9221de7f978a344873c
Thomas Gideon <thomas@webs.com>
2015-05-06 14:24:34 -0400

    Add comments to store and routes.

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ var jira = require('../lib/jira');
 var Promise = require('es6-promise').Promise;
 
 module.exports = {
+    // this app has no local authentication, instead it immediately tries to get
+    // the board views from Jira to test the provided credentials and
+    // incidentally caches the result
     login: function(req, res) {
         var options = {
             hostname: req.body.hostname,
@@ -22,11 +25,17 @@ module.exports = {
         });
     },
 
+    // returns the views cached by login, the UI disallows hitting this end
+    // point unless logged in
     views: function(req, res) {
+        // TODO handle a direct call without logging in, perhaps a 401?
         res.json(req.session.views);
     },
 
+    // for a provided Jira board, return the ids and names for the available
+    // sprints
     sprints: function(req, res) {
+        // TODO handle a direct call without logging in, perhaps a 401?
         jira.sprints(req.session.options, req.params.id).then(function(result) {
             // TODO cache sprints
             var sprints = result.sprints.map(function(sprint) {
@@ -36,10 +45,13 @@ module.exports = {
         });
     },
 
+    // for a provided sprint, fetch a subset of detailed information for all
+    // included tickets in the sprint
     sprint: function(req, res) {
         jira.sprint(req.session.options, req.params.viewId, req.params.sprintId).then(function(result) {
             // TODO cache sprint data
             var issues = [].concat(result.contents.completedIssues, result.contents.incompletedIssues);
+            // coordinate across all the async calls for issue details
             Promise.all(issues.map(function(sprintIssue) {
                 return jira.issueChangeLog(req.session.options, sprintIssue.id).then(function(issue) {
                     // TODO can this be tightened up, perhaps with lodash?
@@ -59,6 +71,8 @@ module.exports = {
                         return { dwell: dwell, status: interested[0].fromString };
                     });
                 });
+            // filter and map out the info of interest once all tickets are
+            // availalbe so all info can be rendered at once
             })).then(function (values) {
                 var dwells = {};
                 values.forEach(function(issueList) {
